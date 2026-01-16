dbe00644c0566784ab013c9b2982ffea5575dc85
KIWOLY <ikiwoly@gmail.com>
2026-01-16 11:04:32 +0300

    tailwind setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,14 @@
-import { createContext, useState } from "react"
-
-
+import React from "react"
 
 
 function App() {
-
-
   return (
-    <>
-   
+    <><div className="min-h-screen bg-black flex items-center justify-center">
+    <h1 className="text-2xl font-bold text-blue-500"> Hellow Tailwind </h1>
+    </div>
+      
 
-    
+
     </>
   )
 }
