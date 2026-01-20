143f3514b0eb32352d86ac510dc9fe2dc34c3981
KIWOLY <ikiwoly@gmail.com>
2026-01-20 14:14:16 +0300

    Jwt settings

--- Backend/accounts/views.py
+++ Backend/accounts/views.py
@@ -3,12 +3,17 @@ from .serializers import UserSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
    
-
+class ProfileView(APIView):
+    def get(self, request):
+        response={"message":"This is a protected profile view."}
+        return Response(response)
+    
 
--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from accounts.views import RegisterView
+from accounts.views import RegisterView, ProfileView
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
